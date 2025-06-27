d7012a2dd12f3e79473f627b9a9f834f06402288
aabuiluk <andrii.a.builuk@gmail.com>
2025-06-27 16:44:26 +0300

    Fix dublication

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ def main():
     bot = TelegramBot(bot_token)
 
     # Демонстрація: просто тестові виклики, те що потім можна використати для unit тестів
-    print(bot.handle_message("/help", 100, 1))
-    print(bot.handle_message("/dice", 100, 2))
-    print(bot.handle_message("/notebook add buy milk", 100, 2))
-    print(bot.handle_message("/notebook list", 100, 2))
-    print(bot.handle_message("/currency", 100, 1))
-    print(bot.handle_message("hello badword", 100, 2))  # буде заблоковано
+    # print(bot.handle_message("/help", 100, 1))
+    # print(bot.handle_message("/dice", 100, 2))
+    # print(bot.handle_message("/notebook add buy milk", 100, 2))
+    # print(bot.handle_message("/notebook list", 100, 2))
+    # print(bot.handle_message("/currency", 100, 1))
+    # print(bot.handle_message("hello badword", 100, 2))  # буде заблоковано
 
     bot.run()
 
