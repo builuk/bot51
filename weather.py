adba10e6e60b2cb25677bbbf4f6b51610f03e2d3
ximok <ximok2007@gmail.com>
2025-08-06 23:13:36 +0200

    upd

---
+++ weather.py
@@ -0,0 +1,6 @@
+# bot/commands/weather.py
+
+class WeatherCommand:
+    def execute(self, text, chat_id, user_id):
+        # У реальності тут буде API запит
+        return "Погода в Києві: ☀️ +25°C"
